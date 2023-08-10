aa6277d400979ee72fa7d22ec9e388975dc6af9c
Rostyslav <rostik.costylyow@gmail.com>
2023-08-10 22:01:54 +0200

    added PHP script to sending mail forms

--- src/js/script.js
+++ src/js/script.js
@@ -83,4 +83,25 @@ $(document).ready(function(){
     validate('#order form');    
 
     $('input[name=phone]').mask("+38 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).validate()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
   });
